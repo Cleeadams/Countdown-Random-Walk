d98dde4679edb66e356c7743bdc4d997613efe3c
Connor Adams <cleeadacon909@gmail.com>
2022-10-03 21:46:28 -0700

    countdown random walk

---
+++ main.py
@@ -0,0 +1,76 @@
+# Countdown Game
+import random
+import statistics
+
+
+def rand_select(val):
+    n = random.choice([1, 2, 3, 4, 5, 6])
+    sel = random.sample(val, n)
+    return n, sel
+
+
+def sel_operations(N):
+    ops = random.sample([1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4], N-1)
+    return ops
+
+
+def add(v1, v2):
+    val = v1 + v2
+    return val
+
+
+def sub(v1, v2):
+    val = v1 - v2
+    return val
+
+
+def mult(v1, v2):
+    val = v1 * v2
+    return val
+
+
+def div(v1, v2):
+    val = v1 / v2
+    return val
+
+
+target = 1
+values = [97, 2, 3, 5, 7, 13]
+sample_sz = 100
+sample = []
+
+for run in range(sample_sz):
+    trials = 1
+    ans = 0
+    while ans != target:
+        [sz, v] = rand_select(values)
+        print(v)
+        operations = sel_operations(sz)
+        print(operations)
+        if operations != []:
+            for i in range(sz-1):
+                if operations[i] == 1:
+                    ans = add(v[i], v[i+1])
+                elif operations[i] == 2:
+                    ans = sub(v[i], v[i+1])
+                elif operations[i] == 3:
+                    ans = mult(v[i], v[i+1])
+                else:
+                    ans = div(v[i], v[i+1])
+                v[i+1] = ans
+        else:
+            ans = v[0]
+        print(trials)
+        print(ans)
+        if ans == target:
+            print('YES')
+            break
+        else:
+            print('NO')
+        trials += 1
+
+    sample.append(trials)
+
+print(sample)
+print(statistics.mean(sample))
+print(statistics.median(sample))
